b981e0ebb6cb5415f2c39a9420011386e5edbaad
anlov <andrey.orlov.97@gmail.com>
2023-12-14 23:30:47 +0300

    Complete

--- src/main/java/com/anlov/Main.java
+++ src/main/java/com/anlov/Main.java
@@ -1,4 +1,48 @@
 package com.anlov;
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 7;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+    public static void printColor() {
+        int value = 97;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a = 77;
+        int b = 78;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+
+    }
+}
