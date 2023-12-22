b7370bdb432c1b709bf2846a5fc905ae52b5f5b9
anlov <andrey.orlov.97@gmail.com>
2023-12-22 17:55:45 +0300

    LAst commit,maybe

--- src/main/java/com/anlov/Main.java
+++ src/main/java/com/anlov/Main.java
@@ -2,3 +2,8 @@ package com.anlov;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
