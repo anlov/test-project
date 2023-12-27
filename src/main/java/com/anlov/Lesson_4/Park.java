81d4eb514c247ef55aa9d43e8c177e3fbe86bc38
anlov <andrey.orlov.97@gmail.com>
2023-12-28 00:49:33 +0300

    Lesson_4

---
+++ src/main/java/com/anlov/Lesson_4/Park.java
@@ -0,0 +1,10 @@
+package com.anlov.Lesson_4;
+
+public class Park {
+    private String parkName = "DisneyLand";
+    public class Attractions {
+        private String infoAttractions;
+        private String timeWork;
+        private double costAttractions;
+    }
+}
