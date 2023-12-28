6ac7eb5084800dc83ecb3f06358a9396dc393e45
anlov <andrey.orlov.97@gmail.com>
2023-12-28 19:15:45 +0300

    Small edits

--- src/main/java/com/anlov/Lesson_4/Park.java
+++ src/main/java/com/anlov/Lesson_4/Park.java
@@ -6,5 +6,11 @@ public class Park {
         private String infoAttractions;
         private String timeWork;
         private double costAttractions;
+        public Attractions(String infoAttractions,String timeWork,double costAttractions) {
+            this.infoAttractions = infoAttractions;
+            this.timeWork = timeWork;
+            this.costAttractions = costAttractions;
+
+        }
     }
 }
