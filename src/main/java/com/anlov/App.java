f334118c235a64f1ae38921087587f24df6593d8
anlov <andrey.orlov.97@gmail.com>
2024-02-05 23:23:36 +0300

    1

--- src/main/java/com/anlov/App.java
+++ src/main/java/com/anlov/App.java
@@ -2,17 +2,7 @@ package com.anlov;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println(factorial(6) );
-    }
+    public static void main( String[] args ) {
 
-    public static int factorial(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException("Факториал рассчитывается только для положительных чисел");
-        } else if (num == 0 || num == 1) {
-            return 1;
-        }
-        return num * factorial(num - 1);
     }
 }
